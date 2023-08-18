146eee4dad9be80f0570b1356293b042358a4cc9
Shane-Donlon <shane.donlon@live.com>
2023-08-18 15:55:02 +0100

    Add random word function

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import random
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -13,5 +14,15 @@ DATA = SHEET.worksheet("passwords")
 PASSWORDS_DATA = DATA.col_values(1)
 AMER_PASSWORDS = DATA.col_values(2)
 
-print(PASSWORDS_DATA)
-print(AMER_PASSWORDS)
+
+def generate_random_word(data):
+    """Runs random choice 3 times and returns random word as output"""
+    
+    i = 0
+    while i < 3:
+        word = random.choice(data)
+        i += 1
+    return word
+
+random_word = generate_random_word(PASSWORDS_DATA)
+print(random_word)
