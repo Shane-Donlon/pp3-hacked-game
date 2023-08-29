2d9dc2a5a14d7a3a95e1234e3ee2303c18653a4a
Shane-Donlon <shane.donlon@live.com>
2023-08-29 22:16:37 +0100

    Password on list 0 length password error is now presented

--- run.py
+++ run.py
@@ -205,26 +205,28 @@ def password_in_list(input_string):
     """Takes in a password then verifies
     the data against Google Sheets data"""
     print("checking...")
-    RESET_COLOURS = Style.RESET_ALL
-    on_list = False
-    for password in PASSWORDS_DATA:
-        if input_string == password:
-            BACKGROUND = Back.RED
-            
-            on_list = True
-
-    for password in AMER_PASSWORDS:
-        BACKGROUND = Back.RED
-        if input_string == password:
-            on_list = True
-
-    if on_list is True:
-        print(f"{BACKGROUND}Your password is on the list")
-        print(f"Please consider changing your password {RESET_COLOURS}")
+    if input_string == "" or len(input_string) == 0:
+        print("Please enter a valid password")
+        new_input = getpass("Enter the password to check: ")
+        password_in_list(new_input)
     else:
-        BACKGROUND = Back.GREEN
-        print(f"{BACKGROUND}Your password is NOT on the list {RESET_COLOURS}")
+        RESET_COLOURS = Style.RESET_ALL
+        on_list = False
+        for password in PASSWORDS_DATA:
+            if input_string == password:
+                BACKGROUND = Back.RED   
+                on_list = True
+        for password in AMER_PASSWORDS:
+            BACKGROUND = Back.RED
+            if input_string == password:
+                on_list = True
 
+        if on_list is True:
+            print(f"{BACKGROUND}Your password is on the list{RESET_COLOURS}")
+            print(f"{BACKGROUND}Please consider changing your password {RESET_COLOURS}")
+        else:
+            BACKGROUND = Back.GREEN
+            print(f"{BACKGROUND}Your password is NOT on the list {RESET_COLOURS}")
 
     print("Deleting password")
     del input_string
