a6cf7f3ce6226d614d7e64f01875e4920002f221
Shane-Donlon <shane.donlon@live.com>
2023-08-30 01:47:02 +0100

    Typos corrected, while loop updated to in range loop

--- run.py
+++ run.py
@@ -28,8 +28,8 @@ def generate_random_word(data):
     a random word. The random word is then sent
     to password_hacking_game function"""
 
-    i = 0
-    while i < 3:
+
+    for i in range(3):
         word = random.choice(data)
         i += 1
     return word
